f575c13982a13f4a83efdd2d49e8c78f0c9cf5f1
rhoda-lee <rhodalee.dev@gmail.com>
2025-08-13 15:18:15 +0000

    Add Patient.cs

---
+++ Assignment3/Patient.cs
@@ -0,0 +1,17 @@
+public class Patient
+{
+    public int Id { get; }
+    public string Name { get; }
+    public int Age { get; }
+    public string Gender { get; }
+
+    public Patient(int id, string name, int age, string gender)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+        Gender = gender;
+    }
+
+    public override string ToString() => $"{Name} (ID: {Id}, Age: {Age}, Gender: {Gender})";
+}

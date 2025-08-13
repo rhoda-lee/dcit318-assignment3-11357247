2fed3c02c29118c5919acd63e41430b17000f4ab
rhoda-lee <rhodalee.dev@gmail.com>
2025-08-13 15:18:52 +0000

    Add Prescription.cs

---
+++ Assignment3/Prescription.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Prescription
+{
+    public int Id { get; }
+    public int PatientId { get; }
+    public string MedicationName { get; }
+    public DateTime DateIssued { get; }
+
+    public Prescription(int id, int patientId, string medicationName, DateTime dateIssued)
+    {
+        Id = id;
+        PatientId = patientId;
+        MedicationName = medicationName;
+        DateIssued = dateIssued;
+    }
+
+    public override string ToString() => $"{MedicationName} (ID: {Id}) issued {DateIssued:d} for patient {PatientId}";
+}

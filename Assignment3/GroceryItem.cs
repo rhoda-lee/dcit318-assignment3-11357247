75f8395bbc1312d447f32fd69c3b8f5fb2db240c
rhoda-lee <rhodalee.dev@gmail.com>
2025-08-15 11:21:41 +0000

    Add GroceryItem.cs

---
+++ Assignment3/GroceryItem.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class GroceryItem : IInventoryItem
+{
+    public int Id { get; }
+    public string Name { get; }
+    public int Quantity { get; set; }
+    public DateTime ExpiryDate { get; }
+
+    public GroceryItem(int id, string name, int quantity, DateTime expiryDate)
+    {
+        Id = id; Name = name; Quantity = quantity; ExpiryDate = expiryDate;
+    }
+
+    public override string ToString() => $"Grocery: {Name} (ID:{Id}) Qty:{Quantity} Expiry:{ExpiryDate:d}";
+}
